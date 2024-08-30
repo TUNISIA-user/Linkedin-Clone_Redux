9cae352cf1b7069ec9c7aa9be252c2b7651666ff
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-08-30 17:36:26 +0100

    Update Header.jsx
    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { signOutAPI } from "../redux/actions";
-
+// .
 const Header = (props) => {
   return (
     <Container>
